e2ed3ec3c17f626995e5124c6b70228520ff7c89
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-10 15:16:59 +0100

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import BackgroundAnimation from './components/BackgroundAnimation'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-slate-100 selection:bg-pink-400 selection:text-pink-900 relative">
+      <BackgroundAnimation />
+      <Navbar />
+      <main className="max-w-screen-xl mx-auto px-6 md:px-12 lg:px-24 relative z-10">
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
